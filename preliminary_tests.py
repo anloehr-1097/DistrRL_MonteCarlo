7aeef40ab8a6e6495c8dbf259947cc76b33f83ac
Andreas Loehr <andreas.loehr97@gmail.com>
2024-04-10 10:34:44 -0400

    adjusted apply_projection to account for numerical imprecision

--- preliminary_tests.py
+++ preliminary_tests.py
@@ -354,12 +354,9 @@ def apply_projection(func: Callable) -> Callable:
 
         for i in range(no_of_bins):
             new_probs[i] = np.sum(kwargs["probs"][bins == i])
-            # TODO: ensure that probs sum to 1, other behviour
 
-        rem: np.float64 = 1 - np.sum(new_probs)
-        new_probs[random.randint(0, no_of_bins - 1)] += rem
+        new_probs = new_probs / np.sum(new_probs)
 
-        # aufsummierren, durh  Summe  teilen
 
         return bin_values, new_probs
 
