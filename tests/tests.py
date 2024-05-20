b2e7eb43a5551a5cadcbcd6c1310d493072cea38
aloehr-1097 <andreas.loehr97@gmail.com>
2024-05-20 13:35:57 +0200

    test dir added

---
+++ tests/tests.py
@@ -0,0 +1,36 @@
+"""Simple testing."""
+
+import numpy as np
+from typing import Tuple
+# from ..preliminary_tests import categorical_projection
+# from .. import preliminary_tests as ptests
+# from ..preliminary_tests import categorical_projection
+from ..preliminary_tests import categorical_projection
+
+
+
+def test_cat_proj():
+    """Test categorical projection."""
+    particles: np.ndarray = np.linspace(0, 10, 11)
+    values: np.ndarray = np.linspace(-0.5, 10.5, 12)
+    print(particles)
+    print(values)
+    probs: np.ndarray = np.ones(np.size(values))
+    probs = probs / np.sum(probs)
+    # probs: np.ndarray = np.random.random(20)
+    # probs = probs / np.sum(probs)
+    print(probs)
+    # assert np.isclose(np.sum(probs), 1), "Probs do not sum to 1." 
+    new_distr: Tuple[np.ndarray, np.ndarray] = categorical_projection(
+        (values, probs), particles)
+
+    assert np.isclose(np.sum(new_distr[1]), 1), "not a prob distribution after projection."
+    return None
+
+def run_tests():
+    test_cat_proj()
+    return None
+
+if __name__ == "__main__":
+    test_cat_proj()
+    print("Everything passed")
