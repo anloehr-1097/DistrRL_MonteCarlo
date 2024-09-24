FIx problem with numerical isntability searchsorted

src/preliminary_tests.py
@@ -159,6 +159,7 @@ class RV:
         """Initialize discrete random variable."""
         assert isinstance(xk, np.ndarray) and isinstance(pk, np.ndarray), \
             "Not numpy arrays upon creation of RV_Discrete."
+        assert xk.size == pk.size, "Size mismatch in xk and pk."
         self.xk: np.ndarray = xk
         self.pk: np.ndarray = pk
         self.is_sorted: bool = False
@@ -202,9 +203,13 @@ class RV:
 
     def qf(self, u: float) -> float:
         """Evaluate quantile function."""
-        if not self.is_sorted:
-            self._sort_njit() if NUMBA_SUPPORT else self._sort()
-        return self.xk[np.searchsorted(self.pk, u) + 1]
+        if np.isclose(u, 1): return self.xk[-1]
+        elif np.isclose(u, 0): return self.xk[0]
+        else:
+            if not self.is_sorted:
+                self._sort_njit() if NUMBA_SUPPORT else self._sort()
+            return self.xk[(np.searchsorted(np.cumsum(self.pk), u))]
+            # TODO this does not work due to numerical isntability
 
 
 class ReturnDistributionFunction:

tests/test_random_variable.py
@@ -4,21 +4,39 @@ import numpy as np
 from typing import Tuple
 import unittest
 from src.preliminary_tests import RV
+import logging
 
-DEBUG: bool = False
+DEBUG: bool = True 
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 class TestFiniteRV(unittest.TestCase):
     def setUp(self):
-        self.rv = RV(
-            xk=np.arange(0, 10, 10),
-            pk=(np.ones(10) / 10),
-        )
+        vals = np.arange(1, 11, 1)
+        probs = np.ones(10) * (1/10)
+        self.rv = RV(vals, probs)
+
+        if DEBUG:
+            logger.info(f"Random variable: {self.rv.distr()}")
         return None
 
     def test_cdf(self):
         cdf_eval = self.rv.cdf(3)
-        self.assertTrue(np.isclose(cdf_eval, 0.3),
-                        "CDF evaluation failed.")
+        self.assertTrue(
+            np.isclose(cdf_eval, 0.3),
+            f"CDF evaluation failed.\nExpected: 0.3\nGot: {cdf_eval}")
 
-
+    def test_qf(self):
+        qf_eval_0 = self.rv.qf(0)
+        qf_eval_1 = self.rv.qf(0.35)
+        qf_eval_2 = self.rv.qf(0.8)
+        qf_eval_3 = self.rv.qf(1.0)
+        qf_eval = np.array([qf_eval_0, qf_eval_1, qf_eval_2, qf_eval_3])
+        if DEBUG: logger.info(f"Quantile function evaluation: {qf_eval}")
+        expected = np.array([1, 4, 8, 10])
+        
+        self.assertTrue(
+            np.isclose(qf_eval, expected).all(),
+            f"Quantile function evaluation failed.\nExpected: {expected}\nGot: {qf_eval}")